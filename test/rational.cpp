cdadc782c183ec47148ccb387833035d95195336
evlsyx <hyetal007@gmail.com>
2019-05-16 02:22:49 +0800

    Added Rational Test Case

--- test/rational.cpp
+++ test/rational.cpp
@@ -9,54 +9,68 @@ TEST(rational, add)
     Rational num = Rational(2,10);
     num += Rational(2,10);
     num += Rational(1,10);
-    EXPECT_EQ(num, Rational(1,2));
 
-    EXPECT_EQ(Rational(2,10) + Rational(3,10), Rational(1,2));
-    EXPECT_EQ(Rational(5,10) + Rational(5,10), Rational(1,1));
-    EXPECT_EQ(Rational() + Rational(7,10), Rational(7,10));
+    EXPECT_EQ(num, Rational(1,2));                                  // addition assignment operator
+    EXPECT_EQ(Rational(2,10) + Rational(3,10), Rational(1,2));      // addition with normalisation
+    EXPECT_EQ(Rational(3,10) + Rational(3,8), Rational(27,40));     // addition with different denominator
+    EXPECT_EQ(Rational(5,10) + Rational(5,10), Rational(1,1));      // addition with whole number as answer
+    EXPECT_EQ(Rational() + Rational(7,10), Rational(7,10));         // addition with zero
 }
 
 TEST(rational, subtract)
 {
-    Rational num = Rational(2,10);
-    num += Rational(2,10);
-    num += Rational(1,10);
-
-    EXPECT_EQ(Rational(3,10) - Rational(2,10), Rational(1,10));
-    EXPECT_EQ(num, Rational(5,10));
+    Rational num = Rational(10,10);
+    num -= Rational(5,10);
+    num -= Rational(2,10);
+    
+    EXPECT_EQ(num, Rational(3,10));                                 // subtraction assignment operator
+    EXPECT_EQ(Rational(6,10) - Rational(4,10), Rational(1,5));      // subtraction with normalisation
+    EXPECT_EQ(Rational(3,10) - Rational(3,8), Rational(-3,40));     // subtraction with different denominator
+    EXPECT_EQ(Rational(5,10) - Rational(5,10), Rational());         // subtraction with zero as answer
+    EXPECT_EQ(Rational(2,10) - Rational(), Rational(1,5));          // subtraction with zero
 }
 
 TEST(rational, multiply)
 {
-    Rational res;
-    res = Rational(3,5) - Rational(1,2);
-    ASSERT_EQ(res, Rational(2,3));
+    Rational num = Rational(10,10);
+    num *= Rational(1,2);
+    num *= Rational(1,2);
+    
+    EXPECT_EQ(num, Rational(1,4));                                  // multiplication assignment operator
+    EXPECT_EQ(Rational(1,10) * Rational(7,10), Rational(7,100));    // multiplication with normalisation
+    EXPECT_EQ(Rational(3,10) * Rational(3,8), Rational(9,80));      // multiplication different denominator
+    EXPECT_EQ(Rational(9,10) * Rational(-9,10), Rational(-81,100)); // multiplication with negative number
+    EXPECT_EQ(Rational(5,10) * Rational(), Rational());             // multiplication with zero
 }
 
 TEST(rational, divide)
 {
-    Rational res;
-    res = Rational(3,5) - Rational(1,2);
-    ASSERT_EQ(res, Rational(2,3));
+    Rational num = Rational(10,10);
+    num /= Rational(1,2);
+    num /= Rational(1,2);
+    
+    EXPECT_EQ(num, Rational(100,1));                                // division assignment operator
+    EXPECT_EQ(Rational(5,10) / Rational(9,10), Rational(-5,9));     // division with normalisation
+    EXPECT_EQ(Rational(4,10) / Rational(1,8), Rational(16,5));      // division different denominator
+    EXPECT_EQ(Rational(1,10) / Rational(-2,10), Rational(-1,2));    // division with negative number
 }
 
 TEST(rational, more_than)
 {
-    Rational res;
-    res = Rational(3,5) - Rational(1,2);
-    ASSERT_EQ(res, Rational(2,3));
+    EXPECT_TRUE(Rational(2,2) > Rational(1,2));                     // more than  
+    EXPECT_TRUE(Rational(1,2) >= Rational(1,2));                    // more than or equal to
+    EXPECT_FALSE(Rational(0,2) > Rational(1,2));                    // not more than            
 }
 
 TEST(rational, less_than)
 {
-    Rational res;
-    res = Rational(3,5) - Rational(1,2);
-    ASSERT_EQ(res, Rational(2,3));
+    EXPECT_TRUE(Rational(1,2) < Rational(2,2));                     // less than  
+    EXPECT_TRUE(Rational(1,2) <= Rational(1,2));                    // less than or equal to
+    EXPECT_FALSE(Rational(3,2) < Rational(1,2));                    // not less than        
 }
 
 TEST(rational, equal)
 {
-    Rational res;
-    res = Rational(3,5) - Rational(1,2);
-    ASSERT_EQ(res, Rational(2,3));
+    EXPECT_TRUE(Rational(1,2) == Rational(1,2));                     // equal to  
+    EXPECT_TRUE(Rational(1,2) != Rational(2,2));                     // not equal to        
 }
