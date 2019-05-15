89c5440cf47e3e5fc4af8030f6204b11375a0d42
evlsyx <hyetal007@gmail.com>
2019-05-16 01:41:27 +0800

    Added Google Test Integration

--- test/rational.cpp
+++ test/rational.cpp
@@ -9,11 +9,11 @@ TEST(rational, add)
     Rational num = Rational(2,10);
     num += Rational(2,10);
     num += Rational(1,10);
-    ASSERT_EQ(num, Rational(1,2));
+    EXPECT_EQ(num, Rational(1,2));
 
-    ASSERT_EQ(Rational(2,10) + Rational(3,10), Rational(1,2));
-    ASSERT_EQ(Rational(5,10) + Rational(5,10), Rational(1,1));
-    ASSERT_EQ(Rational() + Rational(7,10), Rational(7,10));
+    EXPECT_EQ(Rational(2,10) + Rational(3,10), Rational(1,2));
+    EXPECT_EQ(Rational(5,10) + Rational(5,10), Rational(1,1));
+    EXPECT_EQ(Rational() + Rational(7,10), Rational(7,10));
 }
 
 TEST(rational, subtract)
@@ -22,8 +22,8 @@ TEST(rational, subtract)
     num += Rational(2,10);
     num += Rational(1,10);
 
-    ASSERT_EQ(Rational(3,10) - Rational(2,10), Rational(1,10));
-    ASSERT_EQ(num, Rational(5,10));
+    EXPECT_EQ(Rational(3,10) - Rational(2,10), Rational(1,10));
+    EXPECT_EQ(num, Rational(5,10));
 }
 
 TEST(rational, multiply)
